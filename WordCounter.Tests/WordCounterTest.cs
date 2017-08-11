d272ae9a19349c17027a0cd765129dd671297150
Lois Choi <loisch22@gmail.com>
2017-08-11 08:40:09 -0700

    set up files, add obj/bin to .gitignore

---
+++ WordCounter.Tests/WordCounterTest.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class RepeatCounterTest
+  {
+    [TestMethod]
